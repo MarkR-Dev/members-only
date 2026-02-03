caabc56ffb15e64695919f0e5770e0166a8c73c8
Mark Rodgers <markrodgers.dev@gmail.com>
2026-02-03 17:32:17 +0000

    feat: add basic nav

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,4 +5,12 @@ const indexRouter = Router();
 
 indexRouter.get("/", getIndexMessages);
 
+indexRouter.get("/sign-up", (req, res) => {
+  res.send("sign up");
+});
+
+indexRouter.get("/login", (req, res) => {
+  res.send("login");
+});
+
 module.exports = indexRouter;
