66fab9c9a0bf0043484ec2e87231717d847fa24b
Mark Rodgers <markrodgers.dev@gmail.com>
2026-03-02 18:01:20 +0000

    feat: add username join in message db query

--- db/queries.js
+++ db/queries.js
@@ -29,7 +29,7 @@ async function createNewMessage(messageData) {
 // The date/timestamp is stored in the DB via the NOW() function but I didn't want that format to display in the view, decided to format it here when retrieving from the DB, orders the messages by id in descending order to have the newest created messages at the top. For the future, theres probably better ways to handle this
 async function getAllMessages() {
   const { rows } = await pool.query(
-    `SELECT id, title, message, account_id, to_char(date_posted, 'DD/MM/YYYY HH24:MI:SS') AS formatted_date FROM messages ORDER BY id DESC;`,
+    `SELECT messages.id, messages.title, messages.message, accounts.username, to_char(messages.date_posted, 'DD/MM/YYYY HH24:MI:SS') AS formatted_date FROM messages JOIN accounts ON messages.account_id = accounts.id ORDER BY id DESC;`,
   );
   return rows;
 }
